37e4a3d39ffc7e8170bfd5d96006f04af5347dc5
Rusho Shaw <rusho.shaw@gmail.com>
2017-11-19 06:17:47 -0800

    fix

--- src/main/java/com/cts/application/service/TokenService.java
+++ src/main/java/com/cts/application/service/TokenService.java
@@ -1,15 +1,11 @@
 package com.cts.application.service;
 
 import java.io.IOException;
-import java.util.Properties;
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
